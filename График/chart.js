f2ecf32b09098f379da7982572f76ccd82ee7e06
Dendiuser <76583476+Dendiuser@users.noreply.github.com>
2021-02-20 08:37:52 +0200

    Add files via upload

---
+++ График/chart.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(230, 88, 132)',
+            borderColor: 'rgb(270, 140, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
